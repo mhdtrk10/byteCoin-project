77505592be39a75147ce75bddf720023d9051128
mhdtrk10 <99149415+mhdtrk10@users.noreply.github.com>
2024-05-14 19:44:29 +0300

    API kullanarak netten veri çekme
    API kullanarak coin doviz gibi değerleri çekerek uygulamamızda kullanıyoruz.

---
+++ ByteCoin/Model/CoinData.swift
@@ -0,0 +1,14 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Mehdi Oturak on 13.05.2024.
+//  Copyright © 2024 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Decodable {
+    
+    let rate: Double
+}
